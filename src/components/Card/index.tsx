1cf5adeb067fd60a5d25126346625d6a0cefcc06
dev-bomdong <dongdh02076@gmail.com>
2022-04-21 23:45:47 +0900

    add : 이미지 hover 효과 추가

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -17,14 +17,44 @@ const CardImg = styled.img`
 	object-fit: cover;
 	width: 100%;
 	height: auto;
+
+	${CardContainer}:hover & {
+		webkit-filter: blur(2px);
+		filter: blur(2px);
+		transition: filter 0.3s;
+	}
+`;
+
+const CardName = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+	z-index: 1;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(90, 0, 10, 0.4);
+	color: #fff;
+	font-size: 1.5em;
+	font-weight: bold;
+	opacity: 0;
+
+	&:hover {
+	  color: white;
+		font-size: 1.5em
+		transform: scale(1.1);
+		opacity: 1;
+		transition: opacity 0.3s;
+	}
 `;
 
 const Card = ({ data }: Props) => {
 	return (
 		<CardContainer>
-			{/* <img src={data?.image?.url} alt={data?.image?.id} /> */}
-
 			<CardImg src={data?.image?.url} />
+			<CardName>{data.name}</CardName>
 		</CardContainer>
 	);
 };
